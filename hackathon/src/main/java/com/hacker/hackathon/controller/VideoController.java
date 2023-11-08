11ef65cded048212024a21558db988ad9cb929e9
ysbc1247 <ystc1247@khu.ac.kr>
2023-11-08 23:47:26 +0900

    [feat] implemented apis for video

---
+++ hackathon/src/main/java/com/hacker/hackathon/controller/VideoController.java
@@ -0,0 +1,32 @@
+package com.hacker.hackathon.controller;
+
+import com.hacker.hackathon.common.response.ApiResponse;
+import com.hacker.hackathon.dto.VideoViewDTO;
+import com.hacker.hackathon.model.Video;
+import com.hacker.hackathon.service.VideoService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+public class VideoController {
+    private final VideoService videoService;
+
+    public VideoController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+    @PostMapping("/video")
+    public ApiResponse<Video>  uploadVideo(@RequestParam String link){
+        return videoService.uploadVideo(link);
+    }
+
+    @GetMapping("/video/{videoId}")
+    public ApiResponse<Video> getVideo(@PathVariable Long videoId){
+        return videoService.getVideo(videoId);
+    }
+
+    @PostMapping("/user/{userId}/video/{videoId}")
+    public ApiResponse<VideoViewDTO> viewVideo(@PathVariable Long userId, @PathVariable Long videoId){
+        return videoService.viewVideo(userId, videoId);
+    }
+}
