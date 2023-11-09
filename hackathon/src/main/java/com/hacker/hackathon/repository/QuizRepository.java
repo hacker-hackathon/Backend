6b652ecd7eaa5099d7ea372e0c82625602abb8f7
ysbc1247 <ystc1247@khu.ac.kr>
2023-11-10 01:06:03 +0900

    [add] functions in repository added

--- hackathon/src/main/java/com/hacker/hackathon/repository/QuizRepository.java
+++ hackathon/src/main/java/com/hacker/hackathon/repository/QuizRepository.java
@@ -4,6 +4,9 @@ import com.hacker.hackathon.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface QuizRepository extends JpaRepository<Quiz,Long> {
+    List<Quiz> findByVideo_VideoId(Long videoId);
 }
