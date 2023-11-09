04ce94bbcd589654f28dc196d557abd9a4d3aa61
ysbc1247 <ystc1247@khu.ac.kr>
2023-11-09 18:45:58 +0900

    [add] function in repository

--- hackathon/src/main/java/com/hacker/hackathon/repository/UserTodoListRepository.java
+++ hackathon/src/main/java/com/hacker/hackathon/repository/UserTodoListRepository.java
@@ -4,6 +4,9 @@ import com.hacker.hackathon.model.UserTodoList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface UserTodoListRepository extends JpaRepository<UserTodoList,Long> {
+    List<UserTodoList> findByUsers_UsersId(Long userId);
 }
