a9927aadc6abe7ef7a1fdaec519d2de12491fe05
ysbc1247 <ystc1247@khu.ac.kr>
2023-11-10 06:25:17 +0900

    [add] made get todolist function return videoid and quizid values list

--- hackathon/src/main/java/com/hacker/hackathon/service/TodoListService.java
+++ hackathon/src/main/java/com/hacker/hackathon/service/TodoListService.java
@@ -3,12 +3,13 @@ package com.hacker.hackathon.service;
 import com.hacker.hackathon.common.response.ApiResponse;
 import com.hacker.hackathon.common.response.SuccessMessage;
 import com.hacker.hackathon.dto.TodoListAllDTO;
-import com.hacker.hackathon.model.TodoList;
+import com.hacker.hackathon.model.*;
 import com.hacker.hackathon.repository.TodoListRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,27 +23,52 @@ public class TodoListService {
     public Optional<TodoListAllDTO> getTodoListById(Long todoListId) {
         return todoListRepository.findById(todoListId).map(this::convertToTodoListAllDTO);
     }
-    public ApiResponse<List<TodoListAllDTO>> getTodoList(){
+    public ApiResponse<List<TodoListAllDTO>> getTodoList() {
         List<TodoListAllDTO> todoListAllDTOS = todoListRepository.findAll().stream()
-                .map(todoList -> new TodoListAllDTO(
-                        todoList.getTodoListId(),
-                        todoList.getName(),
-                        todoList.getDescription(),
-                        todoList.getTodoVideos(),
-                        todoList.getTodoQuizzes()))
+                .map(todoList -> {
+                    Set<Long> videoIds = todoList.getTodoVideos().stream()
+                            .map(TodoVideo::getVideo)
+                            .map(Video::getVideoId)
+                            .collect(Collectors.toSet());
+
+                    Set<Long> quizIds = todoList.getTodoQuizzes().stream()
+                            .map(TodoQuiz::getQuiz)
+                            .map(Quiz::getQuizId)
+                            .collect(Collectors.toSet());
+
+                    return new TodoListAllDTO(
+                            todoList.getTodoListId(),
+                            todoList.getName(),
+                            todoList.getDescription(),
+                            videoIds,
+                            quizIds);
+                })
                 .collect(Collectors.toList());
-        return ApiResponse.success(SuccessMessage.TODO_LIST_GET_SUCCESS,todoListAllDTOS);
+        return ApiResponse.success(SuccessMessage.TODO_LIST_GET_SUCCESS, todoListAllDTOS);
     }
+
 
     private TodoListAllDTO convertToTodoListAllDTO(TodoList todoList) {
         TodoListAllDTO dto = new TodoListAllDTO();
         dto.setTodoListId(todoList.getTodoListId());
         dto.setName(todoList.getName());
         dto.setDescription(todoList.getDescription());
-        dto.setTodoVideos(todoList.getTodoVideos());
-        dto.setTodoQuizzes(todoList.getTodoQuizzes());
+
+        Set<Long> videoIds = todoList.getTodoVideos().stream()
+                .map(TodoVideo::getVideo)
+                .map(Video::getVideoId)
+                .collect(Collectors.toSet());
+        dto.setTodoVideos(videoIds);
+
+        Set<Long> quizIds = todoList.getTodoQuizzes().stream()
+                .map(TodoQuiz::getQuiz)
+                .map(Quiz::getQuizId)
+                .collect(Collectors.toSet());
+        dto.setTodoQuizzes(quizIds);
+
         return dto;
     }
+
     public ApiResponse<Optional<TodoListAllDTO>> getTodoListbyId(Long listId){
         return ApiResponse.success(SuccessMessage.TODO_LIST_GET_BY_ID_SUCCESS,todoListRepository.findById(listId).map(this::convertToTodoListAllDTO));
     }
