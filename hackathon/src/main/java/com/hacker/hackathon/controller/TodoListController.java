5c21b3a9f19f19aa0fcb73fbea94c056a9c84caf
ysbc1247 <ystc1247@khu.ac.kr>
2023-11-10 01:05:08 +0900

    [fix] functions in todolistcontroller

--- hackathon/src/main/java/com/hacker/hackathon/controller/TodoListController.java
+++ hackathon/src/main/java/com/hacker/hackathon/controller/TodoListController.java
@@ -1,36 +1,31 @@
 package com.hacker.hackathon.controller;
 
 import com.hacker.hackathon.common.response.ApiResponse;
-import com.hacker.hackathon.dto.TodoListDTO;
-import com.hacker.hackathon.dto.TodoListUpdateDTO;
-import com.hacker.hackathon.model.UserTodoList;
+import com.hacker.hackathon.dto.TodoListAllDTO;
 import com.hacker.hackathon.service.TodoListService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TodoListController {
-
     private final TodoListService todoListService;
 
     public TodoListController(TodoListService todoListService) {
         this.todoListService = todoListService;
     }
 
-    @GetMapping("/user/{userId}/list")
-    public ApiResponse<List<TodoListDTO>> getTodoListAll(@PathVariable Long userId){
-        return todoListService.getTodoListByUser(userId);
+    @GetMapping("/list")
+    public ApiResponse<List<TodoListAllDTO>> getTodoList(){
+        return todoListService.getTodoList();
     }
 
-    @GetMapping("/user/{userId}/list/{listId}")
-    public ApiResponse<UserTodoList> getTodoList(@PathVariable Long userId, @PathVariable Long listId){
-        return todoListService.getTodoList(userId, listId);
-    }
-
-    @PutMapping("/user/{userId}/list/{listId}")
-    public ApiResponse<UserTodoList> updateTodoList(@PathVariable Long userId, @PathVariable Long listId, @ModelAttribute TodoListUpdateDTO todoListUpdateDTO){
-        return todoListService.updateTodoList(userId, listId, todoListUpdateDTO);
+    @GetMapping("/list/{listId}")
+    public ApiResponse<Optional<TodoListAllDTO>> getTodoListById(@PathVariable Long listId){
+        return todoListService.getTodoListbyId(listId);
     }
 
 
